c4b844c3fddbf0bbd96573025028ece1e015f4b3
Andrew Paettie <paettiea@gmail.com>
2020-06-30 01:24:23 -0700

    wroking poc for quarkus apigw proxy

---
+++ quarkus/quarkus-api-gw-proxy/src/main/java/quarkus/data/BookRepository.java
@@ -0,0 +1,30 @@
+package quarkus.data;
+
+import quarkus.controller.Book;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Singleton
+public class BookRepository {
+    public static final BookSaved defaultBook = new BookSaved("Hitchhiker's Guide", "42");
+    private static final List<BookSaved> books = new ArrayList<BookSaved>(){{
+        add(defaultBook);
+    }};
+
+    public BookSaved saveBook(Book toSave){
+        BookSaved saved = new BookSaved();
+        saved.setIsbn(UUID.randomUUID().toString());
+        saved.setName(toSave.getName());
+        return saved;
+    }
+
+    public Optional<BookSaved> findById(String id){
+        return books.stream()
+                .filter(b -> b.getIsbn().equals(id))
+                .findFirst();
+    }
+}
