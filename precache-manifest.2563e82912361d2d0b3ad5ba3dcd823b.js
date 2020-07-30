b38701acd66a7bde8da184d07e0c0dfe1ca9a321
Andrew Paettie <paettiea@gmail.com>
2020-07-29 23:36:20 -0700

    Updates

---
+++ precache-manifest.2563e82912361d2d0b3ad5ba3dcd823b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c028bd407cceb7da545d837e67eb0be3",
+    "url": "/aws-lambda-sam-performance/index.html"
+  },
+  {
+    "revision": "cb108d261f6bb5d27ed6",
+    "url": "/aws-lambda-sam-performance/static/css/main.7e58e3ef.chunk.css"
+  },
+  {
+    "revision": "f8d7550242cfacb18299",
+    "url": "/aws-lambda-sam-performance/static/js/2.55e0fa7d.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/aws-lambda-sam-performance/static/js/2.55e0fa7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb108d261f6bb5d27ed6",
+    "url": "/aws-lambda-sam-performance/static/js/main.cd51cd59.chunk.js"
+  },
+  {
+    "revision": "51aecf53b22db98085e7",
+    "url": "/aws-lambda-sam-performance/static/js/runtime-main.20dfb115.js"
+  },
+  {
+    "revision": "71cd9000b41b64d667288d3f45ae0000",
+    "url": "/aws-lambda-sam-performance/static/media/sam-logo.71cd9000.png"
+  }
+]);
